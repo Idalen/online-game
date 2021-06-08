69748d3efc52a364a3a3b42f357ba73a03838806
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-06-08 13:16:38 -0300

    Changed formatting and added some comments
    Comments are NOT done!

--- main.c
+++ main.c
@@ -8,44 +8,44 @@
 #define CASAS 50
 #define LADOS 6
 
-int main() {
+int main()
+{
 
     srand(time(NULL));
 
     int winner = -1;
     int cavalos[JOGADORES];
-    memset(cavalos, 0, JOGADORES*4);
+    memset(cavalos, 0, JOGADORES * 4);
 
-    while( winner == -1 ) {
+    while (winner == -1)
+    {
 
         printf("CORRIDA DE CAVALOS\n");
 
-        for(int i=0; i<JOGADORES; i++) {
+        for (int i = 0; i < JOGADORES; i++)
+        {
 
-            for(int j=0; j<CASAS; j++) {
+            for (int j = 0; j < CASAS; j++)
+            {
 
-                if(cavalos[i] == j)
+                if (cavalos[i] == j)
                     printf("C%d", i);
                 else
                     printf("-");
-
             }
 
             int dice = (rand() % LADOS) + 1;
             cavalos[i] += dice;
 
-            if( cavalos[i] >= CASAS )
+            if (cavalos[i] >= CASAS)
                 winner = i;
 
             printf("\n");
-
         }
 
         sleep(1);
         system("clear");
-
     }
 
     printf("VENCEDOR: %d\n", winner);
-
 }
--- server.c
+++ server.c
@@ -1,7 +1,7 @@
-#include <stdio.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -17,81 +17,89 @@
 #define CASAS 30
 #define LADOS 6
 
-typedef struct user_info{
-  char* client_adress;
+typedef struct user_info
+{
+  char *client_adress;
   int client_socket;
   int user_id;
-  int* pos;
-  int* is_ready; 
-  int* ready;
-  int* round;
-  int* win;
+  int *pos;
+  int *is_ready;
+  int *ready;
+  int *round;
+  int *win;
 } user_info;
 
-typedef struct package{
+typedef struct package
+{
   int pos[4];
   int ready;
   int win;
-}package;
+} package;
 
 void *handle_connection();
 int playersAreReady(int *players);
 int playersArentReady(int *players);
 int winner(int *players);
 
-int main(){
+int main()
+{
 
-  int  client_sock;
+  int client_sock;
   struct sockaddr_in addr;
   int server_sock;
-  server_sock = socket(AF_INET,SOCK_STREAM,0);
+  server_sock = socket(AF_INET, SOCK_STREAM, 0);
 
   struct sockaddr_in cli_addr;
   socklen_t cli_addr_len;
-  char cli_address[MAXLINE+1];
+  char cli_address[MAXLINE + 1];
 
-  if(server_sock == -1){
+  if (server_sock == -1)
+  {
     printf("Error creating socket\n");
     return 1;
   }
 
-  addr.sin_family      = AF_INET;
-  addr.sin_port        = htons(PORT);
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(PORT);
   addr.sin_addr.s_addr = INADDR_ANY;
-  memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
+  memset(&addr.sin_zero, 0, sizeof(addr.sin_zero));
 
-  int yes=1;
+  int yes = 1;
 
-  if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-      perror("setsockopt");
-      exit(1);
+  if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1)
+  {
+    perror("setsockopt");
+    exit(1);
   }
 
-  if(bind(server_sock,(struct sockaddr*)&addr,sizeof(addr)) == -1){
+  if (bind(server_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+  {
     printf("Binding error\n");
     return 1;
   }
 
-  if(listen(server_sock,JOGADORES) == -1){
+  if (listen(server_sock, JOGADORES) == -1)
+  {
     printf("Listening error!\n");
     return 1;
   }
 
   printf("Waiting client to connect on port %d...\n", PORT);
-  
-  
-  int users = 0, round=0;
-  int *pos = malloc(JOGADORES*sizeof(int)), *is_ready = malloc(JOGADORES*sizeof(int)), *ready = malloc(sizeof(int)), *win = malloc(sizeof(int));
-  memset(pos, 0, sizeof(int)*JOGADORES);
-  memset(pos, 0, sizeof(int)*JOGADORES);
+
+  int users = 0, round = 0;
+  int *pos = malloc(JOGADORES * sizeof(int)), *is_ready = malloc(JOGADORES * sizeof(int)), *ready = malloc(sizeof(int)), *win = malloc(sizeof(int));
+  memset(pos, 0, sizeof(int) * JOGADORES);
+  memset(pos, 0, sizeof(int) * JOGADORES);
   *ready = 0;
   *win = -1;
 
-  while(users < JOGADORES){
+  while (users < JOGADORES)
+  {
 
-    client_sock = accept(server_sock, (struct sockaddr_in*)&cli_addr, &cli_addr_len);
+    client_sock = accept(server_sock, (struct sockaddr_in *)&cli_addr, &cli_addr_len);
 
-    if(client_sock == -1){
+    if (client_sock == -1)
+    {
       printf("Client not accepted!\n");
       return 1;
     }
@@ -102,12 +110,12 @@ int main(){
     user_info *p_info = malloc(sizeof(user_info));
     p_info->client_adress = cli_address;
     p_info->client_socket = client_sock;
-    p_info->user_id       = users++;
-    p_info->pos           = pos;
-    p_info->is_ready      = is_ready;
-    p_info->ready         = ready;
-    p_info->round         = &round;
-    p_info->win           = win;
+    p_info->user_id = users++;
+    p_info->pos = pos;
+    p_info->is_ready = is_ready;
+    p_info->ready = ready;
+    p_info->round = &round;
+    p_info->win = win;
 
     pthread_t t;
 
@@ -121,27 +129,30 @@ int main(){
   printf("Jogo iniciado\n");
 
   // Runs while there is no winner
-  do{
+  do
+  {
 
     // Controls ready state
-    if( playersAreReady(is_ready) && *ready == 0) {
+    if (playersAreReady(is_ready) && *ready == 0)
+    {
       round++;
       *ready = 1;
-
-    } else if( playersArentReady(is_ready) && *ready==1) {
+    }
+    else if (playersArentReady(is_ready) && *ready == 1)
+    {
 
       *ready = 0;
-
     }
 
     *win = winner(pos);
 
-  }while(*win == -1); 
+  } while (*win == -1);
 
   printf("Winner: %d\n", *win);
 
   printf("Terminating the game");
-  for(int i=0; i<3; i++){
+  for (int i = 0; i < 3; i++)
+  {
     printf(".");
     sleep(3);
   }
@@ -153,45 +164,45 @@ int main(){
   free(win);
 
   return 0;
-
 }
 
-void* handle_connection(void* p_info){
-  
-  user_info info = *(user_info*) p_info;
+void *handle_connection(void *p_info)
+{
+
+  user_info info = *(user_info *)p_info;
   free(p_info);
 
-  char* client_adress = info.client_adress;
+  char *client_adress = info.client_adress;
   int client_sock = info.client_socket;
   int id = info.user_id;
-  int* pos = info.pos;
-  int* is_ready = info.is_ready; 
-  int* ready = info.ready;
-  int* round = info.round;
+  int *pos = info.pos;
+  int *is_ready = info.is_ready;
+  int *ready = info.ready;
+  int *round = info.round;
   int byte_len, seed = 1, result;
-  int* win = info.win;
+  int *win = info.win;
 
   package p;
-  
+
   byte_len = send(client_sock, &id, sizeof(int), 0);
 
   do
   {
     byte_len = recv(client_sock, &seed, sizeof(int), 0);
-    
-    srand((result+client_sock)*seed);
-    result = rand()%6 + 1;
+
+    srand((result + client_sock) * seed);
+    result = rand() % 6 + 1;
 
     is_ready[id] = 1;
-    pos[id]+=result;
+    pos[id] += result;
 
-    while( !(*ready) ) {
+    while (!(*ready))
+    {
 
       // wait
-
     }
 
-    for(int i=0; i<JOGADORES; i++)
+    for (int i = 0; i < JOGADORES; i++)
       p.pos[i] = pos[i];
     p.ready = *ready;
     p.win = *win;
@@ -199,47 +210,49 @@ void* handle_connection(void* p_info){
     byte_len = send(client_sock, &p, sizeof(package), 0);
 
     printf("Round %d:\n", *round);
-    for(int i = 0; i<JOGADORES; i++)
+    for (int i = 0; i < JOGADORES; i++)
       printf("Horse %d: pos %d \n", i, pos[i]);
 
     is_ready[id] = 0;
 
-  }while (*win == -1);
-  
+  } while (*win == -1);
 }
 
-int playersAreReady(int *players) {
+int playersAreReady(int *players)
+{
 
-  for(int i=0; i<JOGADORES; i++) {
+  for (int i = 0; i < JOGADORES; i++)
+  {
 
-    if( !players[i] ) return 0;
-
+    if (!players[i])
+      return 0;
   }
 
   return 1;
-
 }
 
-int playersArentReady(int *players) {
+int playersArentReady(int *players)
+{
 
-  for(int i=0; i<JOGADORES; i++) {
+  for (int i = 0; i < JOGADORES; i++)
+  {
 
-    if( players[i] ) return 0;
-
+    if (players[i])
+      return 0;
   }
 
   return 1;
-
 }
 
-int winner(int *players) {
+int winner(int *players)
+{
 
-  for(int i=0; i<JOGADORES; i++) {
+  for (int i = 0; i < JOGADORES; i++)
+  {
 
-    if( players[i] >= CASAS ) return i;
-
+    if (players[i] >= CASAS)
+      return i;
   }
 
   return -1;
-
 }

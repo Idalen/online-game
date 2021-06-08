098805991ac6c21ce35ae737cd085c79b6507edd
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-06-08 13:34:44 -0300

    client.c is fully commented

--- client.c
+++ client.c
@@ -18,9 +18,9 @@
 #define LADOS 6
 
 // Struct do package, contendo:
-//  - 4 posicoes
-//  - variavel ready que serve como flag de prontidao
-//  - variavel win que demarca vitoria no jogo
+//  - int[4] para as 4 posicoes de cada um dos jogadores
+//  - int ready que serve como flag de prontidao
+//  - int win que demarca vitoria no jogo
 typedef struct package
 {
   int pos[4];
@@ -28,21 +28,25 @@ typedef struct package
   int win;
 } package;
 
+// Funcao de imprimir os detalhes do jogo na tela para o cliente
+//  INPUT: void
+//  OUTPUT: void (jogo-cliente eh tratado inteiramente dentro da main)
 int main()
 {
-
+  // Inicializando as variaveis para conexao
   int client_sock = socket(AF_INET, SOCK_STREAM, 0);
-  int done = 1;
   struct sockaddr_in addr;
+  int done = 1;
 
   void imprimirJogo(int *jogadores);
 
-  if (client_sock == -1)
+  if (client_sock == -1) // Erro no socket
   {
     printf("Error creating socket\n");
     return 1;
   }
 
+  // Atribuindo valores para as variaveis do address
   addr.sin_family = AF_INET;
   addr.sin_port = htons(PORT);
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -51,7 +55,7 @@ int main()
 
   printf("Connecting to server...\n");
 
-  if (connect(client_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+  if (connect(client_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1) // Erro na conexao com servidor
   {
     printf("Can't connect.\n");
     return 1;
@@ -59,25 +63,26 @@ int main()
 
   printf("Client connected!\n");
 
+  // Inicializando as variaveis usadas no jogo e na comunicacao cliente-servidor
   int bytes_rec, bytes_send, id, pos = 0, seed;
   bytes_rec = recv(client_sock, &id, sizeof(int), 0);
 
   printf("You are horse %d\n", id);
 
-  package p;
+  package p; // Package que sera enviado back-and-forth em cliente->servidor
   do
   {
     printf("Insira tua jogada:\n");
-    scanf("%d", &seed);
+    scanf("%d", &seed); // Cada valor enviado sera usado como seed no random
 
-    bytes_send = send(client_sock, &seed, sizeof(int), 0);
+    bytes_send = send(client_sock, &seed, sizeof(int), 0); // Envia sua jogada
     printf("Aguardando todos os jogadores...\n");
-    bytes_rec = recv(client_sock, &p, sizeof(package), 0);
+    bytes_rec = recv(client_sock, &p, sizeof(package), 0); // Recupera a resposta
 
     // printf("Your current position is %d\n", pos);
-    imprimirJogo(p.pos);
+    imprimirJogo(p.pos); // Atualiza a tela com os dados dos players
 
-  } while (p.win == -1);
+  } while (p.win == -1); // Enquanto o int win do package p nao significar vitoria, continue rodando
 
   if (p.win == id) // Caso o player ganhe
     printf("YOU WIN!!!!!!\n");
@@ -95,14 +100,14 @@ void imprimirJogo(int *jogadores)
 
   system("clear");
 
-  for (int i = 0; i < JOGADORES; i++)
+  for (int i = 0; i < JOGADORES; i++) // Uma pista de corrida para cada jogador
   {
 
-    for (int j = 0; j < CASAS; j++)
+    for (int j = 0; j < CASAS; j++) // Varias casas por pista de corrida
     {
 
       if (jogadores[i] == j) // Se a posicao bate com a ocupada pelo jogador i
-        printf("C%d", i);    // Printa o jogador
+        printf("C%d", i);    // Printa o cavalo
       else
         printf("-"); // Printa espaco em branco
     }
